56dd8c2a75d50c93ebc130a7e50e6eb9a73ae800
SirChickenBro <freddiewashington5555@gmail.com>
2026-08-28 00:13:04 -0700

    Implement remote procedure call

--- dv00_unityMutiplayer/Assets/ClientPlayerMove.cs
+++ dv00_unityMutiplayer/Assets/ClientPlayerMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using StarterAssets;
 
-public class ClientPlayerMove : NetworkBehaviour 
+public class ClientPlayerMove : NetworkBehaviour
 {
     [SerializeField] private PlayerInput m_PlayerInput;
     [SerializeField] private StarterAssetsInputs m_StarterAssetsInputs;
@@ -25,7 +25,28 @@ public class ClientPlayerMove : NetworkBehaviour
         {
             m_StarterAssetsInputs.enabled = true;
             m_PlayerInput.enabled = true;
+        }
+
+        if (IsServer)
+        {
             m_ThirdPersonController.enabled = true;
         }
+    }
+
+    [Rpc(target:SendTo.Server)]
+    private void UpdateInputServerRpc(Vector2 move, Vector2 look, bool jump, bool sprint)
+    {
+        m_StarterAssetsInputs.MoveInput(move);
+        m_StarterAssetsInputs.LookInput(look);
+        m_StarterAssetsInputs.JumpInput(jump);
+        m_StarterAssetsInputs.SprintInput(sprint);
+    }
+
+    private void LateUpdate()
+    {
+        if(!IsOwner)
+            return;
+
+        UpdateInputServerRpc(m_StarterAssetsInputs.move, m_StarterAssetsInputs.look, m_StarterAssetsInputs.jump, m_StarterAssetsInputs.sprint);
     }
 }
